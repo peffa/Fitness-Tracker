fe3043f3586a86c1668937ac93cb3ff7a2d7d413
Ryan Peffer <ryanpeffer@icloud.com>
2020-07-28 15:05:23 -0400

    Add files via upload

---
+++ routes/apiRoutes.js
@@ -0,0 +1,66 @@
+const router = require("express").Router();
+const Workout = require("../models/workout.js");
+
+router.get("/api/workouts", (req, res) => {
+    Workout.find()
+        .then(dbWorkout => {
+            res.json(dbWorkout)
+        })
+        .catch(err => {
+            res.json(err);
+        });
+});
+
+router.get("/api/workouts/range", (req, res) => {
+    Workout.find({}).limit(7)
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.json(err);
+        });
+});
+
+router.post("/api/workouts", ({body}, res) => {
+    Workout.create(body)
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.json(err);
+        });
+});
+
+router.put("/api/workouts/:id", ({ body, params }, res) => {
+    Workout.findByIdAndUpdate(
+        params.id,
+        {
+            $push: {
+                exercises: body
+            }
+        },
+        {
+            new: true,
+            runValidators: true
+        }
+    )
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.json(err);
+        });
+});
+
+router.delete("/api/workouts", ({ body }, res) => {
+    Workout.findOneAndDelete(body.id)
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.json(err);
+        });
+});
+
+module.exports = router;
+    
